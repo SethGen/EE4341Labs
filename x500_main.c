ae2a8c225851e865efe917dd594febeb8c76ed79
SethGen <115184573+SethGen@users.noreply.github.com>
2023-01-22 14:47:39 -0600

    Create x500_main.c

---
+++ x500_main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/** x500_main.c
+ *  EE 4341 Lab 0: Setup
+ *  Seth Gentry
+ */
+/* ************************************************************************** */
+#include "io_setup.h"
+void setup(void)
+{
+    io_setup();
+    // include any other setup functions here
+}
+int main(void)
+{
+    setup();
+    
+    delay(100);                  // 100 ms delay as precaution
+    
+    while(1)
+    { 
+        buttons();
+    }
+    return 0;
+}
+/* *****************************************************************************
+ End of File
+ */
