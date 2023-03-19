fa58c29b87004ca12d813591224f8ea42942787f
SethGen <115184573+SethGen@users.noreply.github.com>
2023-03-19 13:45:03 -0500

    Update EE4341Lab04.c

--- EE4341Lab04.c
+++ EE4341Lab04.c
@@ -3,6 +3,27 @@
 */
 #include <xc.h>
 
+//Header File SDMMC.h
+#define FAIL FALSE
+// Init ERROR code definitions
+#define E_COMMAND_ACK 0x80
+#define E_INIT_TIMEOUT 0x81
+#define B_SIZE 512 // data block size
+char data[B_SIZE];
+char buffer[B_SIZE];
+typedef unsigned LBA; // logic block address, 32 bit wide
+void initSD( void); // initializes I/O pins and SPI
+int initMedia( void); // initializes the SD/MMC memory device
+int getCD(); // check card presence
+int getWP(); // check write protection tab
+int readSECTOR ( LBA, char *); // reads a block of data
+int writeSECTOR( LBA, char *); // writes a block of data
+void initData(void);
+/////////////////////////
+//End of header file
+/////////////////////////
+
+
 // I/O definitions
 // NOTE: Do not use the pins given
 // by the MassStorage.pdf file. To find the 
